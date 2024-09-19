9bb05a14ba023c5212732c0f7c744211a2dc358e
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-19 19:01:14 +0300

    Setup express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('works');
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('works');
+})
+
+app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
