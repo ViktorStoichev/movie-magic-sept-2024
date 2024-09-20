8d726bb46f995b427d69c42994ded2d33942577c
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-20 10:15:02 +0300

    Setup Handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const port = 5000;
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('works');
+    res.render('home', {layout: false});
 })
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
