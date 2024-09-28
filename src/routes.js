92dcfb057dc46866e77287948b78ff1e3bb4de4c
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-28 10:48:26 +0300

    Add resources and cast controller and page

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import homeController from './controllers/home-controller.js';
 import movieController from './controllers/movie-controller.js';
+import castController from './controllers/cast-controller.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/movies', movieController);
+router.use('/cast', castController);
 router.all('*', (req, res) => {
     res.render('404');
 })
