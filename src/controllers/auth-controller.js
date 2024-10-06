66b79a5abf89816cddea25dff009bdecd5373e27
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-10-06 09:17:25 +0300

    Add login page

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -12,6 +12,18 @@ router.post('/register', (req, res) => {
 
     authService.register(email, password);
     res.redirect('/auth/login');
-})
+});
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    
+
+    res.redirect('/');
+});
 
 export default router;
--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,10 @@ import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: String,
-    password: String
+    password: {
+        type: String,
+        minLength: [3, 'Your password is too short!'],
+    }
 });
 
 userSchema.pre('save', async function () {
